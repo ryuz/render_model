b193637e9babc0d3b408bc06381b2cf7c07daff5
Ryuji Fuchikami <ryuz@users.sourceforge.jp>
2015-05-01 16:17:01 +0900

    update

--- Render.h
+++ Render.h
@@ -3,7 +3,7 @@
 #ifndef __RYUZ__RENDER_H__
 #define __RYUZ__RENDER_H__
 
-
+#include <assert.h>
 #include <vector>
 #include "opencv2/opencv.hpp"
 
@@ -12,9 +12,183 @@ template <typename T>
 class Render
 {
 public:
-	void		SetImage(cv::Mat img)	{ m_img = img; }
-	cv::Mat&	GetImage(void)			{ return m_img; }
+	// ポリゴンの描画モード
+	enum PolygonMode {
+		TRIANGLES,
+		TRIANGLE_STRIP,
+		TRIANGLE_FAN,
+		QUADS,
+		QUAD_STRIP,
+	};
+
+	// シェーダーパラメータ
+	struct	ShaderParam {
+		cv::Mat_<T>		vector;
+		std::vector<T>	color;
+		std::vector<T>	coordinate;
+	};
 	
+	// コンストラクタ
+	Render()
+	{
+		m_ImageType     = CV_8UC3;
+		m_DepthType     = cv::DataType<T>::type;
+		m_PolygonMode   = TRIANGLES;
+		m_CullingEnable = false;
+		LoadIdentity();
+		SetSize(640, 480);
+		SetTextureNum(1);
+	}
+	
+	// デストラクタ
+	virtual	~Render()	{}
+	
+	// 画像サイズ設定
+	void SetSize(int width, int height)
+	{
+		m_Width  = width;
+		m_Height = height;
+		ClearBuffer();
+	}
+
+	// バッファクリア
+	void ClearBuffer()
+	{
+		m_ImageBuffer = cv::Mat::zeros(m_Height, m_Width, m_ImageType);
+		m_DepthBuffer = cv::Mat::zeros(m_Height, m_Width, m_DepthType);
+	}
+
+	void SetTextureNum(int n)				{ m_Textures.resize(n); }
+	void SetTexture(cv::Mat img, int n=0)	{ m_Textures[n] = img; }
+
+	// バッファ取得
+	cv::Mat&	GetImage(void)			{ return m_ImageBuffer; }
+	cv::Mat&	GetDepth(void)			{ return m_DepthBuffer; }
+	cv::Mat&	GetTexture(int n=0)		{ return m_Textures[n]; }
+	
+	// マトリックス設定
+	void		LoadIdentity(void)			{ m_Matrix = cv::Mat_<T>::eye(4, 4); }
+	void		SetMatrix(cv::Mat_<T> mat)	{ m_Matrix = mat; }
+	void		MulMatrix(cv::Mat_<T> mat)	{ m_Matrix = m_mat * m_Matrix; }
+	
+	// ポリゴン描画
+	void DrawPolygon(std::vector<ShaderParam> vertexes)
+	{
+		ShaderParam		v[3];
+		unsigned int	p = 0;
+		unsigned int	n = vertexes.size();
+		
+		if ( n < 3 ) {
+			return;
+		}
+
+		switch ( m_PolygonMode ) {
+		case TRIANGLES:
+			while ( n - p > 3 ) {
+				v[0] = vertexes[p+0];
+				v[1] = vertexes[p+1];
+				v[2] = vertexes[p+2];
+				DrawTriangle(v);
+				p += 3;
+			}
+			break;
+		
+		case TRIANGLE_STRIP:
+			while ( n - p > 3 ) {
+				v[0] = vertexes[p+0];
+				v[1] = vertexes[p+1];
+				v[2] = vertexes[p+2];
+				DrawTriangle(v);
+				p++;
+			}
+			break;
+
+		case TRIANGLE_FAN:
+			v[0] = vertexes[p++];
+			while ( n - p > 2 ) {
+				v[1] = vertexes[p+0];
+				v[2] = vertexes[p+1];
+				DrawTriangle(v);
+				p++;
+			}
+			break;
+
+		case QUADS:
+			while ( n - p > 4 ) {
+				v[0] = vertexes[p+0];
+				v[1] = vertexes[p+1];
+				v[2] = vertexes[p+2];
+				DrawTriangle(v);
+				v[0] = vertexes[p+0];
+				v[1] = vertexes[p+2];
+				v[2] = vertexes[p+3];
+				DrawTriangle(v);
+				p += 4;
+			}
+			break;
+
+		case QUAD_STRIP:
+			while ( n - p > 4 ) {
+				v[0] = vertexes[p+0];
+				v[1] = vertexes[p+1];
+				v[2] = vertexes[p+2];
+				DrawTriangle(v);
+				v[0] = vertexes[p+1];
+				v[1] = vertexes[p+3];
+				v[2] = vertexes[p+2];
+				DrawTriangle(v);
+				p += 2;
+			}
+			break;
+		}
+	}
+	
+	// 三角形描画
+	void DrawTriangle(ShaderParam vertex[3]) {
+		// パラメータ数取得
+		m_ColorSize      = vertex[0].color.size();
+		m_CoordinateSize = vertex[0].coordinate.size();;
+		
+		assert(vertex[1].color.size() == m_ColorSize);
+		assert(vertex[2].color.size() == m_ColorSize);
+		assert(vertex[1].coordinate.size() == m_CoordinateSize);
+		assert(vertex[2].coordinate.size() == m_CoordinateSize);
+
+		cv::Point_<T>	v[3];
+		cv::Mat_<T>		z[3];
+		for ( int i = 0; i < 3; i ++ ) {
+			// データコピー
+			ShaderParam param = vertex[i];
+			
+			// 頂点シェーダー
+			VertexShader(param);
+
+			// 座標設定
+			v[i].x = param.vector.at<T>(0, 0);
+			v[i].y = param.vector.at<T>(1, 0);
+			
+			// Zパラメータ設定
+			z[i] = cv::Mat_<T>(2+m_ColorSize+m_CoordinateSize, 1);
+			int p = 0;
+			z[i].at<T>(p++, 0) = param.vector.at<T>(2, 0);			// z
+			z[i].at<T>(p++, 0) = (T)1.0 / param.vector.at<T>(3, 0);	// 1/w
+
+			// 色パラメータ設定
+			for ( unsigned j = 0; j < m_ColorSize; j++ ) {
+				z[i].at<T>(p++, 0) = param.color[j];
+			}
+
+			// 座標パラメータ設定(パースペクティブ補正)
+			for ( unsigned j = 0; j < m_CoordinateSize; j++ ) {
+				z[i].at<T>(p++, 0) = param.coordinate[j] / param.vector.at<T>(3, 0);
+			}
+		}
+		Rasteriser(v, z);
+	}
+
+
+protected:
+	// 最大/最小
 	T	Min(T a, T b)	{ return a < b ? a : b; }
 	T	Min(T a, T b, T c) { return Min(Min(a, b), c); }
 	T	Min(T a, T b, T c, T d) { return Min(Min(a, b), Min(c, d)); }
@@ -22,16 +196,29 @@ public:
 	T	Max(T a, T b, T c) { return Max(Max(a, b), c); }
 	T	Max(T a, T b, T c, T d) { return Max(Max(a, b), Max(c, d)); }
 	
-	virtual void PixcelProc(cv::Point pt, cv::Mat_<T> z) = 0;
-//	{
-//		printf("hoge\n");
-//	}
-
-	void Rasterise(cv::Point_<T> v[3], cv::Mat_<T> z[3])
+	// ラスタライザ
+	void Rasteriser(cv::Point_<T> v[3], cv::Mat_<T> z[3])
 	{
+		// 回転方向チェック
+		cv::Vec<T, 2>	v0 = v[1] - v[0];
+		cv::Vec<T, 2>	v1 = v[2] - v[0];
+		if ( v0[1]*v1[0] - v0[0]*v1[1] < 0 ) {
+			if ( m_CullingEnable ) {
+				return;
+			}
+
+			// 要素入れ替え
+			cv::Point_<T>	tmp_v = v[1];
+			cv::Mat_<T>		tmp_z = z[1];
+			v[1] = v[2];
+			z[1] = z[2];
+			v[2] = tmp_v;
+			z[2] = tmp_z;
+		}
+		
 		// 範囲算出
-		int	x0 = (int)(Min(v[0].x, v[1].x, v[2].x, (T)(m_img.cols-1)) + (T)0.5);
-		int	y0 = (int)(Min(v[0].y, v[1].y, v[2].y, (T)(m_img.rows-1)) + (T)0.5);
+		int	x0 = (int)(Min(v[0].x, v[1].x, v[2].x, (T)(m_Width-1)) + (T)0.5);
+		int	y0 = (int)(Min(v[0].y, v[1].y, v[2].y, (T)(m_Height-1)) + (T)0.5);
 		int	x1 = (int)(Max(v[0].x, v[1].x, v[2].x, (T)0) + (T)0.5);
 		int	y1 = (int)(Max(v[0].y, v[1].y, v[2].y, (T)0) + (T)0.5);
 		
@@ -80,7 +267,7 @@ public:
 			cv::Mat_<T>		zx = zy.clone();
 			for ( pt.x = x0; pt.x <= x1; pt.x++ ) {
 				if ( ex[0] >= 0 && ex[1] >= 0 && ex[2] >= 0 ) {
-					PixcelProc(pt, zx);
+					PixelProc(pt, zx);
 				}
 				ex += edy;
 				zx += zdx;
@@ -90,10 +277,65 @@ public:
 		}
 	}
 
-protected:
-	cv::Mat		m_img;
+	// ピクセル処理
+	void PixelProc(cv::Point pt, cv::Mat_<T> z)
+	{
+		ShaderParam param;
+		int			p = 0;
+		
+		// 座標設定
+		param.vector = cv::Mat_<T>(4, 1);
+		param.vector.at<T>(0, 0) = (T)pt.x;
+		param.vector.at<T>(1, 0) = (T)pt.y;
+		param.vector.at<T>(2, 0) = z.at<T>(p++, 0);
+		param.vector.at<T>(3, 0) = (T)1.0;
+		T	w                    = z.at<T>(p++, 0);
+		
+		// 色情報
+		param.color.resize(m_ColorSize);
+		for ( unsigned int i = 0; i < m_ColorSize; i++ ) {
+			param.color[i] = z.at<T>(p++, 0);
+		}
+
+		// 座標情報(パースペクティブ補正)
+		param.coordinate.resize(m_CoordinateSize);
+		for ( unsigned int i = 0; i < m_CoordinateSize; i++ ) {
+			param.coordinate[i] = z.at<T>(p++, 0) / w;
+		}
+
+		// ピクセルシェーダー
+		PixelShader(param);
+	}
+	
+	// バーテックスシェーダー
+	virtual void VertexShader(ShaderParam& param)
+	{
+		param.vector = m_Matrix * param.vector;
+	}
+
+	// ピクセルシェーダー
+	virtual void PixelShader(ShaderParam& param)
+	{
+	}
+	
+	// メンバー変数
+	int						m_Width;
+	int						m_Height;
+	int						m_ImageType;		// 画像バッファタイプ
+	cv::Mat					m_ImageBuffer;		// 画像バッファ
+	int						m_DepthType;		// 深度バッファタイプ
+	cv::Mat					m_DepthBuffer;		// 深度バッファ	
+	std::vector<cv::Mat>	m_Textures;			// テクスチャ
+
+	PolygonMode				m_PolygonMode;		// ポリゴン描画モード
+	bool					m_CullingEnable;	// 表裏カリング
+	cv::Mat					m_Matrix;			// 変換行列
+	unsigned int			m_ColorSize;		// 色パラメータ数
+	unsigned int			m_CoordinateSize;	// 座標パラメータ数
 };
 
 
 #endif	// __RYUZ__RENDER_H__
 
+
+// end of file
--- main.cpp
+++ main.cpp
@@ -6,10 +6,37 @@
 
 class MyRender : public Render<float>
 {
-	void PixcelProc(cv::Point pt, cv::Mat_<float> z) {
-		m_img.at<cv::Vec3b>(pt)[0] = z(0, 0);
-		m_img.at<cv::Vec3b>(pt)[1] = z(1, 0);
-		m_img.at<cv::Vec3b>(pt)[2] = z(2, 0);
+public:
+
+
+protected:
+	// バーテックスシェーダー
+	void VertexShader(ShaderParam& param) {
+		param.vector = m_Matrix * param.vector;
+	}
+
+	// ピクセルシェーダー
+	void PixelShader(ShaderParam& param) {
+		cv::Point pt;
+		pt.x = (int)param.vector.at<float>(0, 0);
+		pt.y = (int)param.vector.at<float>(1, 0); 
+		float	z = param.vector.at<float>(2, 0); 
+
+		// 深度テスト
+		if ( z < m_DepthBuffer.at<float>(pt) ) {
+			return;
+		}
+
+		m_ImageBuffer.at<cv::Vec3b>(pt)[0] = (uchar)param.color[0];
+		m_ImageBuffer.at<cv::Vec3b>(pt)[1] = (uchar)param.color[1];
+		m_ImageBuffer.at<cv::Vec3b>(pt)[2] = (uchar)param.color[2];
+
+		// テクスチャ
+		float	u = param.color[3];
+		float	v = param.color[4];
+		int uu = (int)((m_Textures[0].cols-1) * u + 0.5);
+		int vv = (int)((m_Textures[0].rows-1) * v + 0.5);
+		m_ImageBuffer.at<cv::Vec3b>(pt) = m_Textures[0].at<cv::Vec3b>(vv, uu);
 	}
 };
 
@@ -18,9 +45,42 @@ int main()
 {
 	MyRender	r;
 	
-	cv::Mat img = cv::Mat::zeros(480, 640, CV_8UC3);
-	r.SetImage(img);
+	MyRender::ShaderParam	vertex[3];
 
+//	cv::Mat img = cv::Mat::zeros(480, 640, CV_8UC3);
+//	r.SetImage(img);
+	
+	cv::Mat imgTexture = cv::imread("DSC_0030.jpg");
+	r.SetTexture(imgTexture);
+
+	vertex[0].vector = (cv::Mat_<float>(4, 1) << 100, 100, 0, 1);
+	vertex[0].color.push_back(255);
+	vertex[0].color.push_back(0);
+	vertex[0].color.push_back(0);
+
+	vertex[0].color.push_back(0);
+	vertex[0].color.push_back(0);
+
+	vertex[1].vector = (cv::Mat_<float>(4, 1) << 300, 420, 0, 1);
+	vertex[1].color.push_back(0);
+	vertex[1].color.push_back(255);
+	vertex[1].color.push_back(0);
+
+	vertex[1].color.push_back(1);
+	vertex[1].color.push_back(0);
+
+	vertex[2].vector = (cv::Mat_<float>(4, 1) << 600, 150, 0, 1);
+	vertex[2].color.push_back(0);
+	vertex[2].color.push_back(0);
+	vertex[2].color.push_back(255);
+
+	vertex[2].color.push_back(0);
+	vertex[2].color.push_back(1);
+	
+	r.DrawTriangle(vertex);
+
+
+	/*
 	cv::Point_<float>	v[3];
 	cv::Mat_<float>		z[3];
 	v[0] = cv::Point2f(100, 100);
@@ -29,10 +89,10 @@ int main()
 	z[0] = (cv::Mat_<float>(3, 1) << 255, 0, 0);
 	z[1] = (cv::Mat_<float>(3, 1) << 0, 255, 0);
 	z[2] = (cv::Mat_<float>(3, 1) << 0, 0, 255);
-	
 	r.Rasterise(v, z);
+	*/
 
-	cv::imshow("hoge", img);
+	cv::imshow("hoge", r.GetImage());
 	cv::waitKey();
 
 	return 0;
